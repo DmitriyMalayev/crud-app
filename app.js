35bba69eefddb966b8ad950654f485d2ae1a0e4a
Dmitriy Malayev <dmitriy.malayev@gmail.com>
2024-06-03 21:39:10 -0400

    authentication

--- app.js
+++ app.js
@@ -2,17 +2,19 @@ require("dotenv").config()
 
 const express = require("express") 
 const expressLayout = require("express-ejs-layouts")
-
+const connectDB = require("./server/config/db.js")
 
 const app = express()
 const PORT = 5001 || process.env.PORT; 
+
+connectDB()
+app.use(express.static('public')) 
 
 
 // Templating Engine
 app.use(expressLayout)
 app.set("layout", "./layouts/main")
 app.set("view engine", "ejs")
-
 
 app.use("/", require("./server/routes/main.js"))
 
--- server/routes/main.js
+++ server/routes/main.js
@@ -1,8 +1,17 @@
 const express = require("express")   //required to use router 
 const router = express.Router(); 
 
-router.get("/about", (req, resp) => {
-    resp.send("Hello World")
+router.get("", (req, resp) => {
+    const locals = {
+        title: "NodeJS Blog",
+        description: "Add and Delete Your Posts"
+    }
+    resp.render("index", {locals})
 })
+
+router.get("/about", (req, resp) => {
+    resp.render("about")
+})
+
 
 module.exports = router
